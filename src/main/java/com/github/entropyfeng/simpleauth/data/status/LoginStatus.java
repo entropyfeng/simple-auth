7c4c4f89bdeb72b97732408ae8f4130175df6d2c
entropyfeng <1092758202@qq.com>
2019-07-03 22:42:35 +0800

    Create LoginStatus.java

---
+++ src/main/java/com/github/entropyfeng/simpleauth/data/status/LoginStatus.java
@@ -0,0 +1,19 @@
+package com.github.entropyfeng.simpleauth.data.status;
+
+/**
+ * @author entropyfeng
+ * @date 2019/7/3 22:37
+ */
+public enum LoginStatus {
+    //登录成功
+    SUCCESS,
+    //密码错误
+    PASSWORD_ERROR,
+    //账户被禁用
+    ACCOUNT_DISABLE,
+    //账户不存在
+    ACCOUNT_NOT_EXIST,
+    //尝试登录次数超限
+    ACCOUNT_LOGIN_OUT_OF_LIMITED
+
+}
