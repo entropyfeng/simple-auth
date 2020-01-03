575dd4fc0b14187d21298ff820a305e57161f9b3
entropyfeng <1092758202@qq.com>
2020-01-03 20:19:04 +0800

    add some constant

--- my-auth/src/main/java/com/github/entropyfeng/myauth/config/AuthProperties.java
+++ my-auth/src/main/java/com/github/entropyfeng/myauth/config/AuthProperties.java
@@ -6,6 +6,9 @@ package com.github.entropyfeng.myauth.config;
  */
 public class AuthProperties {
 
+    /**
+     * Resource的方法名与URL的连接字符串
+     */
     public static final String RESOURCE_METHOD_JOINT="==";
 
     /**
@@ -13,4 +16,16 @@ public class AuthProperties {
      */
     public static final String SALT="}@&_6QdP,.>OmCdB";
 
+    /**
+     * 存放在Redis中的角色前缀
+     */
+    public static final String AUTH_ROLE_SUFFIX="";
+
+
+    /**
+     * HttpRequest 中需要读取的token名称
+     */
+    public static final String AUTH_TOKEN_NAME="auth_token";
+
+    public static final String JWT_SECRET_KEY="e7QW8LL;qe$^&^*&!#$>?wq";
 }
